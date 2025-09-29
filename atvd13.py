f7a7530ee8813f43bddafc671c50a12c4d0d1ecb
Guilherme Abade <abade.guim13@gmail.com>
2025-09-29 20:08:20 -0300

    Finalizando atividades modulo 1

---
+++ atvd13.py
@@ -0,0 +1,7 @@
+lista_numeros = []
+
+for i in range(0, 5):
+    numero = int(input('Digite um numero inteiro: '))
+    lista_numeros.append(numero)
+
+print(f'Lista de numeros inseridos: {lista_numeros}')
