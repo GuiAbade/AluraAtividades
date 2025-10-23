173aaaabfacc41a17848077d0734e5e39a226b68
Guilherme Abade <abade.guim13@gmail.com>
2025-10-22 23:12:32 -0300

    Alura Data Science

---
+++ Alura Data Science 3/atvd1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from random import choice
+
+lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+print(f"Número escolhido: {choice(lista)}")
+
+
+# Sorteia um número aleatório da lista
